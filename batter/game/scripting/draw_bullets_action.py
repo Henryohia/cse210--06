ea0f0aac5c0c533f0f54355be2fec5360385891b
mbencomo2 <matt.ibencomo@gmail.com>
2022-12-08 12:42:51 -0700

    bullets draw on the screen now

---
+++ batter/game/scripting/draw_bullets_action.py
@@ -0,0 +1,22 @@
+from constants import *
+from game.scripting.action import Action
+
+
+class DrawBulletsAction(Action):
+
+    def __init__(self, video_service):
+        self._video_service = video_service
+        
+    def execute(self, cast, script, callback):
+        bullets = cast.get_actors(BULLET_GROUP)
+        
+        for bullet in bullets:
+          body = bullet.get_body()
+
+          if bullet.is_debug():
+              rectangle = body.get_rectangle()
+              self._video_service.draw_rectangle(rectangle, PURPLE)
+              
+          image = bullet.get_image()
+          position = body.get_position()
+          self._video_service.draw_image(image, position)
