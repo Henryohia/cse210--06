8ec0eae735a97788bfcaf91ac65986bb311defcd
mbencomo2 <matt.ibencomo@gmail.com>
2022-12-08 12:47:36 -0700

    bullets move vertically up the screen

---
+++ batter/game/scripting/move_bullets_action.py
@@ -0,0 +1,18 @@
+from constants import *
+from game.scripting.action import Action
+
+
+class MoveBulletsAction(Action):
+
+    def __init__(self):
+        pass
+        
+    def execute(self, cast, script, callback):
+        bullets = cast.get_actors(BULLET_GROUP)
+        for bullet in bullets:
+          body = bullet.get_body()
+          position = body.get_position()
+          velocity = body.get_velocity()
+          position = position.add(velocity)
+          body.set_position(position)
+          bullet.get_older()
