f030dd7b5ea278125e20c395dcc84c3edbe37290
mbencomo2 <matt.ibencomo@gmail.com>
2022-12-10 03:56:32 -0700

    updated fielpaths and other constants

--- Demolisher_final/demolisher/constants.py
+++ Demolisher_final/demolisher/constants.py
@@ -6,7 +6,7 @@ from game.casting.color import Color
 # -------------------------------------------------------------------------------------------------- 
 
 # GAME
-GAME_NAME = "Batter"
+GAME_NAME = "Demolisher"
 FRAME_RATE = 60
 
 # SCREEN
@@ -20,16 +20,18 @@ FIELD_TOP = 60
 FIELD_BOTTOM = SCREEN_HEIGHT
 FIELD_LEFT = 0
 FIELD_RIGHT = SCREEN_WIDTH
+COLUMNS_WIDTH = 80
+ROW_HEIGHT = 28
 
 # FONT
-FONT_FILE = "batter/assets/fonts/zorque.otf"
+FONT_FILE = "demolisher/assets/fonts/zorque.otf"
 FONT_SMALL = 32
 FONT_LARGE = 48
 
 # SOUND
-BOUNCE_SOUND = "batter/assets/sounds/boing.wav"
-WELCOME_SOUND = "batter/assets/sounds/start.wav"
-OVER_SOUND = "batter/assets/sounds/over.wav"
+BOUNCE_SOUND = "demolisher/assets/sounds/boing.wav"
+WELCOME_SOUND = "demolisher/assets/sounds/start.wav"
+OVER_SOUND = "demolisher/assets/sounds/over.wav"
 
 # TEXT
 ALIGN_CENTER = 0
@@ -54,10 +56,11 @@ TRY_AGAIN = 1
 NEXT_LEVEL = 2
 IN_PLAY = 3
 GAME_OVER = 4
+VICTORY = 5
 
 # LEVELS
-LEVEL_FILE = "batter/assets/data/level-{:03}.txt"
-BASE_LEVELS = 5
+LEVEL_FILE = "demolisher/assets/data/level-{:03}.txt"
+BASE_LEVELS = 4
 
 # -------------------------------------------------------------------------------------------------- 
 # SCRIPTING CONSTANTS
@@ -92,39 +95,38 @@ SCORE_FORMAT = "SCORE: {}"
 
 # BALL
 BALL_GROUP = "balls"
-BALL_IMAGES = [f"batter/assets/images/{i:03}.png" for i in range(0, 5)]
-BALL_WIDTH = 56
-BALL_HEIGHT = 56
+BALL_IMAGES = [f"demolisher/assets/images/{i:03}.png" for i in range(0, 5)]
+BALL_WIDTH = 54
+BALL_HEIGHT = 54
 BALL_VELOCITY = 6
 
 # RACKET
 RACKET_GROUP = "rackets"
-RACKET_IMAGE = "batter/assets/images/100.png"
-RACKET_WIDTH = 46
-RACKET_HEIGHT = 54
-RACKET_RATE = 6
+RACKET_IMAGE = "demolisher/assets/images/100.png"
+RACKET_WIDTH = 45
+RACKET_HEIGHT = 10
 RACKET_VELOCITY = 10
 
 #Bullets
 BULLET_GROUP = "bullets"
-BULLET_IMAGES = [f"batter/assets/images/{i:03}.png" for i in range(200, 202)]
+BULLET_IMAGES = [f"demolisher/assets/images/{i:03}.png" for i in range(200, 202)]
 BULLET_WIDTH = 14
 BULLET_HEIGHT = 28
-PLAYER_BULLET_VELOCITY = 6
-ALIEN_BULLET_VELOCITY = -6
+PLAYER_BULLET_VELOCITY = -6
+ALIEN_BULLET_VELOCITY = 4
 
 # BRICK
 BRICK_GROUP = "bricks"
 BRICK_IMAGES = {
-    "s": "batter/assets/images/010.png",
-    "m": "batter/assets/images/020.png",
-    "l": "batter/assets/images/030.png",
-    "x": "batter/assets/images/040.png",
-    "e": "batter/assets/images/050.png",
-    "a": "batter/assets/images/060.png"
+    "s": "demolisher/assets/images/010.png",
+    "m": "demolisher/assets/images/020.png",
+    "l": "demolisher/assets/images/030.png",
+    "x": "demolisher/assets/images/040.png",
+    "e": "demolisher/assets/images/050.png",
+    "a": "demolisher/assets/images/060.png"
 }
-BRICK_WIDTH = 80
-BRICK_HEIGHT = 28
+BRICK_WIDTH = 30
+BRICK_HEIGHT = 30
 BRICK_DELAY = 0.5
 BRICK_RATE = 4
 BRICK_POINTS = 50
@@ -134,3 +136,4 @@ DIALOG_GROUP = "dialogs"
 ENTER_TO_START = "PRESS ENTER TO START"
 PREP_TO_LAUNCH = "PREPARING TO LAUNCH"
 WAS_GOOD_GAME = "GAME OVER"
+WAS_Victory = "CONGLATURATIONS!"
