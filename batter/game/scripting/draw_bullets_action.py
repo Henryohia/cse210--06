410680a38b4c1b444eb9a0ddf805b19c9d2a248c
mbencomo2 <matt.ibencomo@gmail.com>
2022-12-09 21:47:12 -0700

    fixed formatting

--- batter/game/scripting/draw_bullets_action.py
+++ batter/game/scripting/draw_bullets_action.py
@@ -11,12 +11,12 @@ class DrawBulletsAction(Action):
         bullets = cast.get_actors(BULLET_GROUP)
         
         for bullet in bullets:
-          body = bullet.get_body()
+            body = bullet.get_body()
 
-          if bullet.is_debug():
-              rectangle = body.get_rectangle()
-              self._video_service.draw_rectangle(rectangle, PURPLE)
-              
-          image = bullet.get_image()
-          position = body.get_position()
-          self._video_service.draw_image(image, position)
+            if bullet.is_debug():
+                rectangle = body.get_rectangle()
+                self._video_service.draw_rectangle(rectangle, PURPLE)
+            
+            image = bullet.get_image()
+            position = body.get_position()
+            self._video_service.draw_image(image, position)
